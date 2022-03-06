a153d2d1cc21976e20b4c9506fe3d9685bbc2029
rasitdeveloper <frasitkilic@gmail.com>
2022-03-06 14:36:59 +0300

    Added photo update

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const Photo = require('./models/Photo');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
+const methodOverride = require('method-override');
 
 //connect DB
 mongoose.connect('mongodb://localhost/pcat-test-db', {
@@ -21,6 +22,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 //routes
 app.get('/', async (req,res) => {
@@ -58,6 +60,21 @@ app.post('/photos', async (req,res) => {
         res.redirect('/');
     })
     
+})
+
+app.get('/photos/edit/:id', async (req,res) => {
+    const photo = await Photo.findOne({_id: req.params.id})
+    res.render('edit', {
+        photo
+    });
+})
+
+app.put('/photos/:id', async (req,res) => {
+    const photo = await Photo.findOne({_id: req.params.id});
+    photo.title = req.body.title;
+    photo.description = req.body.description;
+    await photo.save()
+    res.redirect(`/photos/${req.params.id}`);
 })
 
 const port = 3000;
